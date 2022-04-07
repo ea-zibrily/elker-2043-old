90f78574a3438c125f0a69e58d25ed3cc4c82f1b
Zibrrr <zyuuannn@gmail.com>
2022-04-07 10:39:07 +0700

    Fix Bug Movement
    yahaha hayyu

---
+++ Assets/Script/Ele Script/ele.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ele : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    [SerializeField] public float jumpH;
+    [SerializeField] public bool isDirectionRight;
+    Rigidbody2D rb;
+
+    [SerializeField] public float radius;
+    [SerializeField] public Transform groundChecker;
+    [SerializeField] public LayerMask ifIsGround;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        print("Welcome to ELKER 2K43!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        eleJump();
+    }
+    private void FixedUpdate()
+    {
+        eleMovement();
+    }
+    void eleMovement()
+    {
+        float move;
+        move = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+        if (move > 0 && !isDirectionRight)
+        {
+            transform.eulerAngles = Vector2.zero;
+            isDirectionRight = true;
+        }
+        else if(move < 0 && isDirectionRight)
+        {
+            transform.eulerAngles = Vector2.up * 180;
+            isDirectionRight = false;
+        }
+    }
+
+    void eleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            //rb.velocity = new Vector2(rb.velocity.x, jumpH * 1);
+            //rb.AddForce(Vector2.up * jumpH);
+            rb.velocity = Vector2.up * jumpH;
+        }
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundChecker.position, radius, ifIsGround);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundChecker.position, radius);
+    }
+}
