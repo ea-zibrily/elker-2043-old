e8ae885860c1dae03d5b4f9509df3052fefe93ef
Zibrrr <zyuuannn@gmail.com>
2022-04-10 11:08:16 +0700

    Coba buat CameMov baru
    Tapi unityku ngebug anjime

---
+++ Assets/Script/CameraMovement/EleCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EleCamera : MonoBehaviour
+{
+    [SerializeField] public Transform ele;
+    private float smoothCam = 0.25f;
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private Vector3 velocity = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 elePosititon =  ele.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, elePosititon, ref velocity, smoothCam);
+    }
+}
